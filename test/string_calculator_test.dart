d4fd113e3ce604586eaae2583050302f6a004978
Tarish Ahmed B <tarishahmed40@gmail.com>
2025-09-13 10:19:41 +0530

    Single number returns the number

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -8,8 +8,16 @@ void main() {
   group('String Calculator', () {
   
   StringCalculator stringCalculator = StringCalculator();
+  // Empty string returns 0
   test('Empty string returns 0', () {
       expect(stringCalculator.calculate(''), 0);
     });
+
+    // Single number returns the number
+    test('Single number returns the number', () {
+      expect(stringCalculator.calculate('1'), 1);
+    });
+    
+    
   });
 }
